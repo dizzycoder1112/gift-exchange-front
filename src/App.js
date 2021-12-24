31b72eea6d6fc2f247207390971c91953c7bdf40
Champer Wu <champer@Champers-MacBook-Pro.local>
2021-12-24 13:24:11 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
+
+
 
 function App() {
+  
+  const [ user, setUser ] = useState(-1);
+  const [ people, setPeople ] = useState([]);
+
+  const [ giftReceiver, setGiftReceiver ] = useState(null);
+  useEffect(() =>{
+    const url = "http://143.244.155.191:8000/readFile";
+    const fetchData = async () => {
+      const response = await fetch(url);
+      const data = await response.json();
+      setPeople(data);
+
+    }
+    fetchData();
+  },[])
+
+  const getGiftReceiver = async () => {
+    console.log(JSON.stringify({useIndex: user}));
+    const url = "http://143.244.155.191:8000";
+    const fetchData = async () => {
+      const response = await fetch( url,
+        {
+          body: JSON.stringify({userIndex: user}),
+          method: "POST",
+          headers: {
+            'user-agent': 'Mozilla/4.0 MDN Example',
+            'content-type': 'application/json'
+          },
+        });
+      const data = await response.json();
+      setGiftReceiver(data);
+    }
+    fetchData();
+  }
+
+  const result = (data) => {
+    if(data !== null){
+      return <div>你抽到了 {data.name}</div>;
+    }
+    return '';
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{textAlign: "center", display: "flex", flexDirection: "column", margin:"0 auto"}}>
+      <div>
+        <label htmlFor="cars">你是誰？:</label>
+      </div>
+      <div>
+        <select value={user} onChange={(e) => { setUser(e.target.value) }} id="cars">
+          <option value={-1}>請選擇</option>
+          {
+            people.map((person, index) => {
+              return <option key={index} value={index}>{index+1}. {person.name}</option>
+            })
+          }
+        </select>
+      </div>
+      <div>
+        <button onClick={() => {getGiftReceiver()}}>抽籤囉!</button>
+      </div>
+      <div>
+        {result(giftReceiver)}
+      </div>
+      
+      
     </div>
   );
 }
